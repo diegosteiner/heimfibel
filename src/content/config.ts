b6dbd1fe1b6a3b734593cb0494190e1996d69b3f
Diego Steiner <diego.steiner@u041.ch>
2024-02-04 10:15:41 +0000

    import 04.02.2024

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ import { locales } from '../i18n';
 const articles = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
-		id: z.number(),
+		id: z.string(),
 		title: z.string(),
 		phase: z.string(),
 		step: z.string(),
