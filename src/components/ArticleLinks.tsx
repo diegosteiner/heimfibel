f18af866c1be6f5e5cd2400c60cd51bb0e54700e
Diego Steiner <diego.steiner@u041.ch>
2024-05-03 12:12:59 +0000

    fix: link list

--- src/components/ArticleLinks.tsx
+++ src/components/ArticleLinks.tsx
@@ -2,9 +2,27 @@ import type { CollectionEntry } from "astro:content";
 import React, { useState, useEffect, useMemo } from "react";
 import type { Locale } from "../i18n";
 
-export function ArticleList({ articles, locale }: { articles: CollectionEntry<"articles">[]; locale: Locale }) {
+type Article = CollectionEntry<"articles">;
+
+export function ArticleList({ articles, locale }: { articles: Article[]; locale: Locale }) {
   const [showAll, setShowAll] = useState(false);
-  const shuffledArticles = useMemo(() => articles.sort(() => 0.5 - Math.random()), [articles])
+  const [shuffledArticles, setShuffledArticles] = useState<Article[]>();
+
+  useEffect(() => setShuffledArticles([...articles].sort(() => 0.5 - Math.random())), [articles]);
+
+  if (!shuffledArticles)
+    return (
+      <ul className="links">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li>
+          <button className="showAll" onClick={() => setShowAll(true)}>
+            &hellip;
+          </button>
+        </li>
+      </ul>
+    );
 
   return (
     <ul className="links">
@@ -16,7 +34,13 @@ export function ArticleList({ articles, locale }: { articles: CollectionEntry<"a
         shuffledArticles
           .slice(3, -1)
           .map((link) => <ArticleLink key={link.id} article={link} locale={locale}></ArticleLink>)}
-      {shuffledArticles.length > 3 && !showAll && <li><button className="showAll" onClick={() => setShowAll(true)}>&hellip;</button></li>}
+      {shuffledArticles.length > 3 && !showAll && (
+        <li>
+          <button className="showAll" onClick={() => setShowAll(true)}>
+            &hellip;
+          </button>
+        </li>
+      )}
     </ul>
   );
 }
